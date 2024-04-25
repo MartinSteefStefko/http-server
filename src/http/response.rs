9f03a7e8b0bfb1e517033f48c414aa98fb0e1fd7
Martin Steef Stefko <mato.stefko@gmail.com>
2024-04-25 23:25:31 +0200

    feat: response and status codes

---
+++ src/http/response.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::io::{Result as IoResult, Write};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use super::StatusCode;
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+    headers: HashMap<String, String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode) -> Self {
+        Response {
+            status_code,
+            body: None,
+            headers: HashMap::new(),
+        }
+    }
+    pub fn set_body(&mut self, body: String, content_type: &str) {
+        let etag = generate_etag(&body);
+        self.headers.insert("Etag".to_string(), etag);
+
+        self.body = Some(body);
+        self.headers
+            .insert("Content-Type".to_string(), content_type.to_string());
+        self.headers.insert(
+            "Content-Length".to_string(),
+            self.body.as_ref().unwrap().len().to_string(),
+        );
+        self.headers
+            .insert("Date".to_string(), format!("{}", http_date()));
+        self.headers.insert(
+            "Cache-Control".to_string(),
+            "public, max-age=0, must-revalidate".to_string(),
+        );
+    }
+
+    pub fn add_header(&mut self, key: &str, value: &str) {
+        self.headers.insert(key.to_string(), value.to_string());
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n",
+            self.status_code,
+            self.status_code.reason_phrase()
+        )?;
+        for (key, value) in &self.headers {
+            write!(stream, "{}: {}\r\n", key, value)?;
+        }
+        write!(
+            stream,
+            "\r\n{}",
+            self.body.as_ref().unwrap_or(&"".to_string())
+        )
+    }
+}
+
+fn http_date() -> String {
+    let now = SystemTime::now();
+    let datetime: chrono::DateTime<chrono::Utc> = now.into();
+    datetime.format("%a, %d %b %Y %H:%M:%S GMT").to_string()
+}
+
+fn generate_etag(body: &str) -> String {
+    let digest = md5::compute(body);
+    format!("\"{:?}\"", digest)
+}
---
+++ src/http/status_code.rs
@@ -0,0 +1,44 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[derive(Copy, Clone, Debug)]
+pub enum StatusCode {
+    Ok = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    MethodNotAllowed = 405,
+    NotAcceptable = 406,
+    InternalServerError = 500,
+    NotImplemented = 501,
+    BadGateway = 502,
+    ServiceUnavailable = 503,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "OK",
+            Self::Created => "Created",
+            Self::NoContent => "No Content",
+            Self::BadRequest => "Bad Request",
+            Self::Unauthorized => "Unauthorized",
+            Self::Forbidden => "Forbidden",
+            Self::NotFound => "Not Found",
+            Self::MethodNotAllowed => "Method Not Allowed",
+            Self::NotAcceptable => "Not Acceptable",
+            Self::InternalServerError => "Internal Server Error",
+            Self::NotImplemented => "Not Implemented",
+            Self::BadGateway => "Bad Gateway",
+            Self::ServiceUnavailable => "Service Unavailable",
+        }
+    }
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", *self as u16)
+    }
+}
